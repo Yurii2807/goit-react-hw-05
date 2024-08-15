014875a0a7b06efad0678330038f1b369110c7fc
Yurii2807 <156546118+Yurii2807@users.noreply.github.com>
2024-08-15 20:50:21 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//vitejs.dev/config/
-https: export default defineConfig({
+//https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
